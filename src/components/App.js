51f9f401343d68dbc3642ab954d78aa3e3ccaf8d
Rajat Kumar <rkumar.cse17@chitkarauniversity.edu.in>
2020-06-08 15:04:55 +0530

    overflow control

--- src/components/App.js
+++ src/components/App.js
@@ -6,21 +6,28 @@ import Search from "./SearchBar";
 import Contacts from "./Contacts";
 
 class App extends React.Component {
-  state = { value: "", formType: "Add", formData: {}, fetch: () => { } }
+  state = { value: "", formType: "Add", formData: {}, fetch: () => {} };
   render = () => {
     return (
       <div style={{ margin: "0 15vw 0 15vw" }}>
         <Segment style={{ marginTop: "2rem" }}>
           <Search setValue={(value) => this.setState({ value })} />
-          <NewForm formType={this.state.formType} formData={this.state.formData} resetType={(user) => {
-            this.setState({ formType: "Add", formData: user });
-            this.state.fetch()
-          }} />
+          <NewForm
+            formType={this.state.formType}
+            formData={this.state.formData}
+            resetType={(user) => {
+              this.setState({ formType: "Add", formData: user });
+              this.state.fetch();
+            }}
+          />
         </Segment>
         <br />
         <Contacts
-          searchValue={this.state.value} setFetch={fetch => this.setState({ fetch })}
-          editRqst={(formType, formData) => this.setState({ formType, formData })}
+          searchValue={this.state.value}
+          setFetch={(fetch) => this.setState({ fetch })}
+          editRqst={(formType, formData) =>
+            this.setState({ formType, formData })
+          }
         />
       </div>
     );
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,35 +1,55 @@
 import React from "react";
-import axios from "../apis/axios"
-import { List, Segment, Icon, Accordion, Pagination, Dimmer, Loader } from "semantic-ui-react";
+import axios from "../apis/axios";
+import {
+  List,
+  Segment,
+  Icon,
+  Accordion,
+  Pagination,
+  Dimmer,
+  Loader,
+} from "semantic-ui-react";
 class Contacts extends React.Component {
   state = {
-    activeIndex: 0, users: [], total: 0, value: "", fetching: false
+    activeIndex: 0,
+    users: [],
+    total: 0,
+    value: "",
+    fetching: false,
   };
 
-  componentDidMount = () => { this.fetchUsers(0); this.props.setFetch(this.fetchUsers) }
+  componentDidMount = () => {
+    this.fetchUsers(0);
+    this.props.setFetch(this.fetchUsers);
+  };
 
   componentDidUpdate = () => {
     if (this.props.searchValue !== this.state.value) {
       // eslint-disable-next-line
       this.state.value = this.props.searchValue;
-      this.fetchUsers(0)
+      this.fetchUsers(0);
     }
-  }
+  };
 
   fetchUsers = async (page) => {
-    this.setState({ fetching: true })
-    page = page ? page : 0
-    let val = this.state.value; val = val.trim(); val = val.replace(/[/^ *$/]{2,}/g, " ");
-    const path = val && val.length ? `/users?page=${page}&value=${val}` : `/users?page=${page}`;
-    const { users, total } = (await axios.get(path)).data
-    if (users)
-      this.setState({ users, total })
-    setTimeout(() => { this.setState({ fetching: false }) }, 500)
-
-  }
+    this.setState({ fetching: true });
+    page = page ? page : 0;
+    let val = this.state.value;
+    val = val.trim();
+    val = val.replace(/[/^ *$/]{2,}/g, " ");
+    const path =
+      val && val.length
+        ? `/users?page=${page}&value=${val}`
+        : `/users?page=${page}`;
+    const { users, total } = (await axios.get(path)).data;
+    if (users) this.setState({ users, total });
+    setTimeout(() => {
+      this.setState({ fetching: false });
+    }, 500);
+  };
   onUserDelete = async (_id, i) => {
-    const msg = (await axios.delete(`/users?_id=${_id}`)).data
-    this.fetchUsers(i < 4 ? 0 : (i + 1) / 4)
+    const msg = (await axios.delete(`/users?_id=${_id}`)).data;
+    this.fetchUsers(i < 4 ? 0 : (i + 1) / 4);
     alert(msg.err ? msg.err : msg);
   };
   onUserEdit = (i) => {
@@ -43,68 +63,86 @@ class Contacts extends React.Component {
 
     this.setState({ activeIndex: newIndex });
   };
-  pageChange = (e, { activePage }) => { this.fetchUsers(activePage - 1); }
+  pageChange = (e, { activePage }) => {
+    this.fetchUsers(activePage - 1);
+  };
   renderUser = (user, i) => {
-
     const { activeIndex } = this.state;
-    return (<Accordion key={user._id} fluid styled>
-      <Accordion.Title
-        active={activeIndex === i}
-        index={i}
-        onClick={this.handleClick}
-      >
-        <Icon name="dropdown" />{user.name}
-      </Accordion.Title>
-      <Accordion.Content active={activeIndex === i}>
-        <Segment>
-          <div style={{ display: "flex" }}>
-            <List>
-              {user.phone.map((no, ind) => <List.Item icon="phone" key={ind} content={no} />)}
-            </List>
-            <div style={{ marginLeft: "15%" }}>
+    return (
+      <Accordion key={user._id} fluid styled>
+        <Accordion.Title
+          active={activeIndex === i}
+          index={i}
+          onClick={this.handleClick}
+        >
+          <Icon name="dropdown" />
+          {user.name}
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === i}>
+          <Segment>
+            <div
+              style={{
+                display: "flex",
+                overflow: "auto",
+                paddingBottom: "1rem",
+              }}
+            >
               <List>
-                {user.email.map((email, ind) => <List.Item icon="mail" key={ind} content={email} />)}
+                {user.phone.map((no, ind) => (
+                  <List.Item icon="phone" key={ind} content={no} />
+                ))}
               </List>
+              <div style={{ marginLeft: "15%" }}>
+                <List>
+                  {user.email.map((email, ind) => (
+                    <List.Item icon="mail" key={ind} content={email} />
+                  ))}
+                </List>
+              </div>
+              <div style={{ position: "relative", width: "100%" }}></div>
+              <div style={{ display: "flex" }}>
+                <Icon
+                  name="delete"
+                  color="red"
+                  size="big"
+                  onClick={() => this.onUserDelete(user._id, i)}
+                />
+                <Icon
+                  name="edit"
+                  color="green"
+                  size="big"
+                  onClick={() => this.onUserEdit(i)}
+                />
+              </div>
             </div>
-            <div style={{ position: "relative", width: "100%" }}></div>
-            <div style={{ display: "flex" }}  >
-              <Icon
-                name="delete"
-                color="red"
-                size="big"
-                onClick={() => this.onUserDelete(user._id, i)}
-              />
-              <Icon
-                name="edit"
-                color="green"
-                size="big"
-                onClick={() => this.onUserEdit(i)}
-              />
-            </div>
-          </div>
-        </Segment>
-      </Accordion.Content>
-    </Accordion>
-    )
-  }
+          </Segment>
+        </Accordion.Content>
+      </Accordion>
+    );
+  };
   render() {
     return (
       <>
-
-        {this.state.fetching ? <Segment style={{ height: "30vh" }}>
-          <Dimmer active inverted>
-            <Loader size='large'>Loading</Loader>
-          </Dimmer>
-        </Segment> : this.state.users.map((user, i) => this.renderUser(user, i))}
+        {this.state.fetching ? (
+          <Segment style={{ height: "30vh" }}>
+            <Dimmer active inverted>
+              <Loader size="large">Loading</Loader>
+            </Dimmer>
+          </Segment>
+        ) : (
+          this.state.users.map((user, i) => this.renderUser(user, i))
+        )}
         <br />
         <center>
-          <Pagination defaultActivePage={0} totalPages={this.state.total / 4} onPageChange={this.pageChange} />
+          <Pagination
+            defaultActivePage={0}
+            totalPages={this.state.total / 4}
+            onPageChange={this.pageChange}
+          />
         </center>
       </>
     );
   }
 }
-
-
 
 export default Contacts;
