d6d072c2e21d6d748683ec4d54de09d86bbd1200
Rajat Kumar <rkumar.cse17@chitkarauniversity.edu.in>
2020-06-09 14:38:05 +0530

    new build

---
+++ build/precache-manifest.d85d798c91e7e74ced08fbb7a203d9b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf46c3f60bf89860f60e344c431d642",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03747874ced527603a32",
+    "url": "/static/js/2.00061721.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/static/js/2.00061721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f49bf73cae2671a373d7",
+    "url": "/static/js/main.8ebe2514.chunk.js"
+  },
+  {
+    "revision": "1e184bed41a810564677",
+    "url": "/static/js/runtime-main.1ca79223.js"
+  }
+]);
